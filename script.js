d9ea3917264d210f9abedcbb2180d3b76e8060ab
Akhilesh-Yadav6283 <135110548+Akhilesh-Yadav6283@users.noreply.github.com>
2023-11-22 15:57:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let firstInput=prompt("Enter the first number");
+  let multiply=firstInput*2;
+let secondInput=prompt("Enter the second number");
+ let result=multiply ++ secondInput;
+alert(`The final result is : ${result});
